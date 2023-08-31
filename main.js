c9d1f96f329b90cd1f297b12b9645cfd38d32b4f
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2023-08-31 12:33:01 -0600

    init

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hi");
+// Canvas Template
+
+// Canvas Setup
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 600;
+cnv.height = 400;
+
+// Call draw function once all page resources have loaded
+window.addEventListener("load", draw);
+
+function draw() {
+  ctx.fillStyle = "green";
+  ctx.fillRect(50, 100, 200, 80);
+
+  // Redraw
+  requestAnimationFrame(draw);
+}
