90b36d8171252f34c78e5c8eef3661ac522340b9
muskanshah-7923 <124912381+muskanshah-7923@users.noreply.github.com>
2024-02-01 11:11:13 -0700

    Draw Door

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Canvas Template
+// Draw Door Function by Muskan 
 
 // Canvas Setup
 let cnv = document.getElementById("my-canvas");
@@ -16,3 +16,39 @@ function draw() {
   // Redraw
   requestAnimationFrame(draw);
 }
+
+function drawDoor(x, y, frameColor) {
+
+}
+
+// Define & Invoke a Function
+
+function displayGreeting() {
+console.log("Hello");
+console.log("This is a function")
+}
+
+// DEFINE A FUNCTION
+
+// PARAMETERS ARE: name & timeOfDay
+function displayGreeting(name, timeOfDay) {
+  console.log('Hello ' + name + '.');
+  console.log('Have a good ' + timeOfDay + '!');
+}
+
+//INVOKE FUNCTION WITH DIFFERENT ARGUMENTS
+displayGreeting('Megan', 'Morning');
+// Arguments are 'Megan' & 'Morning'
+// -> Hello Megan. Have a good Morning!
+
+
+displayGreeting('Azra', 'Afternoon');
+// Arguments are 'Azra' & 'Afternoon'
+// -> Hello Azra. Have a good Afternoon!
+
+
+displayGreeting('Nathan', 'Night');
+// Arguments are 'Nathan' & 'Night'
+// -> Hello Nathan. Have a good Night!
+
+
